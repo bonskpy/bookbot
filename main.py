dc6c85cf24802e70de0bc20f57893c17b778540f
bonskpy <bonskpy@gmail.com>
2025-03-09 13:17:49 +0100

    added option to pass comm line variables as sys.argv

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import sys
 from stats import get_book_text, count_words, character_count 
 from stats import sort_characters
 
 BOOK_PATH = "books/frankenstein.txt"
 
 def main():
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    BOOK_PATH = sys.argv[1]
+
     book_content = get_book_text(BOOK_PATH) 
     words_count = count_words(book_content)
     char_count = character_count(book_content)
@@ -18,7 +26,7 @@ def main():
     for item in sorted_characters:
         for key, value in item.items():
             if key.isalpha():
-                print(key, ":", value)
+                print(f"{key}: {value}")
     print("============= END ===============\n\n")
 
 main()
